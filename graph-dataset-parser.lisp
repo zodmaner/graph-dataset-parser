efb4a13012ef83e94733d888ca4c1ac3297dd98a
Smith Dhumbumroong <zodmaner@gmail.com>
2017-08-14 13:25:05 +0700

    Merged latest changes

--- graph-dataset-parser.lisp
+++ graph-dataset-parser.lisp
@@ -11,7 +11,9 @@
 
 (defconstant +empty-list+ '())
 
-(defparameter *default-vector-size* (expt 10 6))
+(declaim (type (simple-array fixnum (1000000)) *buffer*))
+(defparameter *buffer* (make-array 1000000 :element-type 'fixnum)
+  "The global buffer array; should be large enough to hold all adjacent vertices of any vertex.")
 
 (declaim (ftype (function (simple-string) list) parse-vertex-and-adjacent-vertex))
 (defun parse-vertex-and-adjacent-vertex (line)
@@ -21,18 +23,19 @@ label."
   (mapcar (lambda (s) (parse-integer s :junk-allowed t))
           (split-sequence-if (lambda (c) (or (char= c #\Tab) (char= c #\space))) line)))
 
-(declaim (ftype (function (fixnum (vector fixnum)) null) write-vertex-and-adjacent-vertices))
-(defun write-vertex-and-adjacent-vertices (vertex adjacent-vertices)
+(declaim (ftype (function (fixnum (simple-array fixnum) fixnum) null)
+                write-vertex-and-adjacent-vertices))
+(defun write-vertex-and-adjacent-vertices (vertex adjacent-vertices adjvs-end)
   "Given a vertex and a vector containing adjacent vertices, write
 them out to the *STANDARD-OUTPUT* in a format that's suitable for
 consumption by Pregel+/Palgol applications."
-  (format *standard-output* "~A~A~A ~A" vertex #\Tab vertex (length adjacent-vertices))
-  (loop :for adjv fixnum :across adjacent-vertices :do
-     (format *standard-output* " ~A ~A" adjv vertex))
+  (format *standard-output* "~A~A~A ~A" vertex #\Tab vertex adjvs-end)
+  (loop :for index :from 0 :below adjvs-end :do
+     (format *standard-output* " ~A ~A" (aref adjacent-vertices index) vertex))
   (format *standard-output* "~%"))
 
-(declaim (ftype (function (pathname pathname) null) parse-graph-dataset))
-(defun parse-graph-dataset (src dst)
+(declaim (ftype (function (pathname pathname (simple-array fixnum)) null) parse-graph-dataset))
+(defun parse-graph-dataset (src dst adjvs-buffer)
   "Parse a graph dataset file into a format that's suitable for
 consumption by Pregel+/Palgol applications and write the result out to
 a specified output file."
@@ -42,25 +45,24 @@ a specified output file."
       (let ((*standard-output* out-stream))
         (loop
            :with vertex fixnum := -1
-           :and adjacent-vertices :of-type (vector fixnum) := (make-array *default-vector-size*
-                                                                          :element-type 'fixnum
-                                                                          :fill-pointer 0)
+           :and index fixnum := 0
            :for line :of-type (or simple-string null) := (read-line in-stream nil nil)
            :while line :do
            (when (and (char/= #\# (aref line 0)) (char/= #\% (aref line 0)))
               (multiple-value-bind (v adjv) (values-list (parse-vertex-and-adjacent-vertex line))
                (declare (fixnum v adjv))
                (when (/= vertex v)
-                 (when (/= (length adjacent-vertices) 0)
-                   (write-vertex-and-adjacent-vertices vertex adjacent-vertices))
+                 (when (/= index 0)
+                   (write-vertex-and-adjacent-vertices vertex adjvs-buffer index))
                  (setf vertex v
-                       (fill-pointer adjacent-vertices) 0))
-               (vector-push adjv adjacent-vertices)))
-           :finally (write-vertex-and-adjacent-vertices vertex adjacent-vertices))))))
+                       index 0))
+               (setf (aref adjvs-buffer index) adjv)
+               (incf index)))
+           :finally (write-vertex-and-adjacent-vertices vertex adjvs-buffer index))))))
 
 (declaim (ftype (function (list) null) main))
 (defun main (args)
   "The executable image's toplevel function."
   (if (/= 3 (length args))
       (format t "Usage: ~A SOURCE DEST~%" (first args))
-      (parse-graph-dataset (pathname (second args)) (pathname (third args)))))
+      (parse-graph-dataset (pathname (second args)) (pathname (third args)) *buffer*)))

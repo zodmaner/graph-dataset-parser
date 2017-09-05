66d1a1316d3a8f7ffd02be05a777828309d637d6
Smith Dhumbumroong <zodmaner@gmail.com>
2017-09-05 21:34:43 +0700

    Further simplified the codebase

--- graph-dataset-parser.lisp
+++ graph-dataset-parser.lisp
@@ -12,52 +12,47 @@
   "Parse a graph dataset file into a format that's suitable for
 consumption by Pregel+/Palgol applications and write the result out to
 a specified output file."
-  (flet ((comment-p (line)
-           (declare (simple-string line))
-           (let ((first-char (aref line 0)))
-             (or (char= #\# first-char) (char= #\% first-char))))
-         (parse-line (line)
-           (declare (simple-string line))
-           (-<>> line
-                 (split-sequence-if (lambda (c) (or (char= c #\Tab) (char= c #\space))))
-                 (remove "" <> :test #'string=)
-                 (mapcar (lambda (s) (parse-integer s)))))
-         (write-vertex (v adjvs)
-           (declare (fixnum v)
-                    ((vector fixnum) adjvs))
-           (let ((adjvs-len (length adjvs)))
-             (declare (fixnum adjvs-len))
-             (format *standard-output* "~A~A~A ~A" v #\Tab v adjvs-len)
-             (if (= adjvs-len 0)
-                 (format *standard-output* "~%")
-                 (progn
-                   (loop :for adjv fixnum :across adjvs
-                      :do (format *standard-output* " ~A ~A" adjv v))
-                   (format *standard-output* "~%")))))
-         (add-vertex (v adjv ht)
-           (declare (fixnum v)
-                    ((or fixnum null) adjv))
-           (let ((adjvs (gethash v ht
-                                 (make-array 1 :element-type 'fixnum :fill-pointer 0))))
-             (declare ((vector fixnum) adjvs))
-             (when (and (not (null adjv))
-                        (not (find adjv adjvs)))
-               (vector-push-extend adjv adjvs))
-             (setf (gethash v ht) adjvs))))
-    (declare (inline comment-p
-                     parse-line
-                     write-vertex
-                     add-vertex))
-    (let ((v-adjvs (make-hash-table :size 10000000)))
+  (let ((v-adjvs (make-hash-table :size 10000000)))
+    (flet ((comment-p (line)
+             (declare (simple-string line))
+             (let ((first-char (aref line 0)))
+               (or (char= #\# first-char) (char= #\% first-char))))
+           (parse-line (line)
+             (declare (simple-string line))
+             (-<>> line
+                   (split-sequence-if (lambda (c) (or (char= c #\Tab) (char= c #\space))))
+                   (remove "" <> :test #'string=)
+                   (mapcar (lambda (s) (parse-integer s)))))
+           (add-vertex (v adjv)
+             (declare (fixnum v)
+                      ((or fixnum null) adjv))
+             (let ((adjvs (gethash v v-adjvs
+                                   (make-array 1 :element-type 'fixnum :fill-pointer 0))))
+               (declare ((vector fixnum) adjvs))
+               (when (and (not (null adjv))
+                          (not (find adjv adjvs)))
+                 (vector-push-extend adjv adjvs))
+               (setf (gethash v v-adjvs) adjvs)))
+           (write-vertex (v adjvs)
+             (declare (fixnum v)
+                      ((vector fixnum) adjvs))
+             (format *standard-output* "~A~A~A ~A" v #\Tab v (length adjvs))
+             (loop :for adjv fixnum :across adjvs :do
+                (format *standard-output* " ~A ~A" adjv v)
+                :finally (format *standard-output* "~%"))))
+      (declare (inline comment-p
+                       parse-line
+                       add-vertex
+                       write-vertex))
       (with-open-file (in-stream src)
         (loop :with lr fixnum := 0
            :for line :of-type (or simple-string null) := (read-line in-stream nil nil)
            :while line :do
-           (when (not (comment-p line))             
+           (when (not (comment-p line))
              (multiple-value-bind (v adjv) (values-list (parse-line line))
                (declare (fixnum v adjv))
-               (add-vertex v adjv v-adjvs)
-               (add-vertex adjv nil v-adjvs)
+               (add-vertex v adjv)
+               (add-vertex adjv nil)
                (format *error-output* "~A# of lines read: ~A... " #\Return lr)
                (incf lr)))
            :finally
